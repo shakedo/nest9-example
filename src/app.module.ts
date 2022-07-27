8db23ce6326c4bfa4da4f15f7ab5d4c449682fbc
Ofir Shaked <ofir.shaked@incredibuild.com>
2022-07-27 09:38:17 +0300

    Add rabbitmq client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { RabbitMQModule } from "@golevelup/nestjs-rabbitmq";
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot(),
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'exchange1',
+          type: 'topic',
+        },
+      ],
+      uri: 'amqp://localhost:5672',
+      connectionInitOptions: { wait: false },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
